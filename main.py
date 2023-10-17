d6dde0fd0b48a4a48eb355f03228eea7c8185699
Chen YiJie <coaixy@qq.com>
2023-10-17 23:07:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 import os
-import httpx
-import re
+import requests
 import time
+import re
 
 # CONFIG PART =====================================
 uid = os.environ["UID"]
@@ -25,51 +25,67 @@ header = {
     'Accept-Language': 'zh-CN,zh;q=0.9'
 }
 
-session = httpx.Client()
-r = session.get('http://jhc.sunnysport.org.cn/login/',headers=header)
-# 下面是为了应对set-cookie无效所搞的无奈之举 所以实际上也没有必要使用Client了
-header['Cookie'] = re.match('(sessionid=.*?);',r.headers.get('set-cookie')).group(1)
-print("complete")
-vrf = re.search('name="vrf" value="(.*?)">', r.content.decode()).group(1)
 
-loginData = {
-    'username': uid,
-    'vrf': vrf,
-    'password': password
-}
-
-r = session.post('http://jhc.sunnysport.org.cn/login/',headers=header,data=loginData,allow_redirects=False)
-header['Cookie'] = re.match('(sessionid=.*?);',r.headers.get('set-cookie')).group(1)
-
-# 拿数据
-totalRecord = session.get('http://jhc.sunnysport.org.cn/runner/data/speed.json',headers=header).json()
-print(totalRecord)
-
-# 因为懒得写正则 所以目前的有效次数和有效里程都是从总的数据里直接算的
-totalMileage = 0
-validMileage = 0
-totalTimes = 0
-validTimes = 0
-
-todayRecord = {}
-if time.strftime("%Y-%m-%d", time.localtime()) in totalRecord[len(totalRecord)-1]['runnerTime']:
-    todayRecord = totalRecord[len(totalRecord)-1]
-    for dayRecord in totalRecord:
-        totalMileage += dayRecord['runnerMileage']
-        totalTimes += 1
-        if dayRecord['runnerSpeed'] > minSpeed and dayRecord['runnerMileage'] > minMileage:
-            validMileage += dayRecord['runnerMileage']
-            validTimes += 1
-
-if todayRecord:
-    desp = '今日跑步距离：{}\n\n今日跑步速度：{}\n\n---\n\n'.format(todayRecord['runnerMileage'],round(todayRecord['runnerSpeed'],2))
-    desp += '总里程：{}\n\n总次数：{}\n\n---\n\n'.format(totalMileage,totalTimes)
-    desp += '有效里程：{}\n\n有效次数：{}\n\n---\n\n'.format(validMileage,validTimes)
-    print(desp)
-    # 推数据
-    msg2send = {
-        'title': '阳光长跑记录已更新',
-        'desp': desp
+def spawn_data(user_name, user_pwd, vrf):
+    return {
+        'username': user_name,
+        'vrf': vrf,
+        'password': user_pwd,
+        "userType": "person",
+        "agency": "体育部"
     }
-    httpx.post('https://sctapi.ftqq.com/{}.send'.format(SendKey),data=msg2send)
+
+
+def get_mid_text(text, start_str, end_str):
+    start_index = text.index(start_str)
+    if start_index >= 0:
+        start_index += len(start_str)
+    end_index = text.index(end_str)
+    return text[start_index:end_index]
+
+
+if __name__ == '__main__':
+    session = requests.session()
+    res = session.get("http://jhc.sunnysport.org.cn/login/", data={}, headers=header)
+    header['Cookie'] = res.headers['Set-Cookie'].split(";")[0]
+    html = res.text
+    vrf = re.search('name="vrf" value="(.*?)">', html).group(1)
+    data = spawn_data(uid, password, vrf)
+    res = session.post('http://jhc.sunnysport.org.cn/login/', headers=header, data=data, allow_redirects=False)
+    response_header = res.headers
+    session_id = response_header['set-cookie'].split(";")[0]
+    header['Cookie'] = session_id
     
+    # 拿数据
+    totalRecord = session.get('http://jhc.sunnysport.org.cn/runner/data/speed.json',headers=header).json()
+    print(totalRecord)
+
+    # 因为懒得写正则 所以目前的有效次数和有效里程都是从总的数据里直接算的
+    totalMileage = 0
+    validMileage = 0
+    totalTimes = 0
+    validTimes = 0
+
+    todayRecord = {}
+    if time.strftime("%Y-%m-%d", time.localtime()) in totalRecord[len(totalRecord)-1]['runnerTime']:
+        todayRecord = totalRecord[len(totalRecord)-1]
+        for dayRecord in totalRecord:
+            totalMileage += dayRecord['runnerMileage']
+            totalTimes += 1
+            if dayRecord['runnerSpeed'] > minSpeed and dayRecord['runnerMileage'] > minMileage:
+                validMileage += dayRecord['runnerMileage']
+                validTimes += 1
+
+    if todayRecord:
+        desp = '今日跑步距离：{}\n\n今日跑步速度：{}\n\n---\n\n'.format(todayRecord['runnerMileage'],round(todayRecord['runnerSpeed'],2))
+        desp += '总里程：{}\n\n总次数：{}\n\n---\n\n'.format(totalMileage,totalTimes)
+        desp += '有效里程：{}\n\n有效次数：{}\n\n---\n\n'.format(validMileage,validTimes)
+        print(desp)
+        # 推数据
+        msg2send = {
+            'title': '阳光长跑记录已更新',
+            'desp': desp
+        }
+        httpx.post('https://sctapi.ftqq.com/{}.send'.format(SendKey),data=msg2send)
+
+
